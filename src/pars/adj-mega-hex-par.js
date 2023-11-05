6267f4f77879c6977e9e62449119d630365f0cba
Tom Striker <toms.baugis@gmail.com>
2023-11-05 12:25:43 +0000

    simplify tri and hex config and rename dimmer to brightness as we don't need dimmer when we have RGB

--- src/pars/adj-mega-hex-par.js
+++ src/pars/adj-mega-hex-par.js
@@ -1,191 +1,14 @@
 import {ModelFactory} from "../device.js";
 import {rangeProp} from "../utils.js";
-import {Control} from "../controls.js";
-
-let red = rangeProp({channel: 1, label: "Red"});
-let green = rangeProp({channel: 2, label: "Green"});
-let blue = rangeProp({channel: 3, label: "Blue"});
-let white = rangeProp({channel: 4, label: "White"});
-let amber = rangeProp({channel: 5, label: "Amber"});
-let uv = rangeProp({channel: 6, label: "Ultraviolet"});
-let dimmer = rangeProp({channel: 7, label: "Dimmer", defaultDMXVal: 255});
-
-function between(val, bounds) {
-    return bounds.some(([lower, upper]) => val >= lower && val <= upper);
-}
 
 let strobe = {
-    channel: 8,
-    label: "Strobe",
-    modes: [
-        {chVal: 32, val: "off", label: "Off"},
-        {chVal: 64, val: "on", label: "On"},
-        {chVal: 128, val: "pulse", label: "Pulse"},
-        {chVal: 192, val: "random", label: "Random"},
-    ],
-    defaultDMXVal: 32,
-};
-
-let strobeSpeed = {
-    channel: 8,
-    label: "Strobe Speed",
-    modifies: "strobe",
-    condition: device =>
-        between(device.strobe.dmx, [
-            [64, 95],
-            [128, 159],
-            [192, 223],
-        ]),
+    // even though the tripar is capable of several strobe modes, we default to the standard
+    // as anything more sophisticated can be implemented on software side
+    defaultDMXVal: 63,
     stops: [
-        {chVal: 0, val: 0, label: "Slow"},
-        {chVal: 31, val: 1, label: "Fast"},
-    ],
-};
-
-let programMode = {
-    channel: 9,
-    label: "Program Mode",
-    modes: [
-        {chVal: 0, val: "dimming", label: "No Program"},
-        {chVal: 52, val: "color_macro", label: "Color Macro"},
-        {chVal: 103, val: "color_change", label: "Color Change"},
-        {chVal: 154, val: "color_fade", label: "Color Fade"},
-        {chVal: 205, val: "sound", label: "Sound"},
-    ],
-};
-
-let colorMacro = (() => {
-    // color macros permutate through all 6 combos. the permutation order is shallow rather than deep
-    function powerSet(list) {
-        // https://codereview.stackexchange.com/a/39747
-        let set = [];
-        let listSize = list.length;
-        let combinationsCount = 1 << listSize;
-
-        for (var i = 1; i < combinationsCount; i++) {
-            let combination = [];
-            for (var j = 0; j < listSize; j++) {
-                if (i & (1 << j)) {
-                    combination.push(list[j]);
-                }
-            }
-            set.push(combination);
-        }
-        return set;
-    }
-
-    let components = ["R", "G", "B", "W", "A", "U"];
-    let combos = powerSet(components);
-
-    // i'm bit too dum to figure out how to flip the code above from deep to shallow order
-    // so we do a dumb weighting sort
-    let byOrder = combo => {
-        let weight = combo.reduce((total, comp, idx) => {
-            return total + (components.indexOf(comp) + 1) * Math.pow(10, 6 - idx);
-        }, 0);
-        return weight * Math.pow(10, combo.length);
-    };
-    combos.sort((a, b) => byOrder(a) - byOrder(b));
-    let modes = [
-        {chVal: 0, label: "Off"},
-        ...combos.map((combo, idx) => ({chVal: 4 + idx * 4, label: combo.join(" + ")})),
-    ];
-
-    return {
-        channel: 10,
-        label: "Color Macros",
-        condition: device => between(device.programMode.dmx, [[52, 102]]),
-        modes,
-    };
-})();
-
-let colorChange = {
-    channel: 10,
-    label: "Color Change",
-    condition: device => between(device.programMode.dmx, [[103, 153]]),
-    modes: [
-        {chVal: 0, val: "Mode 1"},
-        {chVal: 16, val: "Mode 2"},
-        {chVal: 32, val: "Mode 3"},
-        {chVal: 48, val: "Mode 4"},
-        {chVal: 64, val: "Mode 5"},
-        {chVal: 80, val: "Mode 6"},
-        {chVal: 96, val: "Mode 7"},
-        {chVal: 112, val: "Mode 8"},
-        {chVal: 128, val: "Mode 9"},
-        {chVal: 144, val: "Mode 10"},
-        {chVal: 160, val: "Mode 11"},
-        {chVal: 176, val: "Mode 12"},
-        {chVal: 192, val: "Mode 13"},
-        {chVal: 208, val: "Mode 14"},
-        {chVal: 224, val: "Mode 15"},
-        {chVal: 240, val: "Mode 16"},
-    ],
-};
-
-let colorFade = {
-    channel: 10,
-    label: "Color Fade",
-    condition: device => between(device.programMode.dmx, [[154, 204]]),
-    modes: [
-        {chVal: 0, val: "Mode 1"},
-        {chVal: 16, val: "Mode 2"},
-        {chVal: 32, val: "Mode 3"},
-        {chVal: 48, val: "Mode 4"},
-        {chVal: 64, val: "Mode 5"},
-        {chVal: 80, val: "Mode 6"},
-        {chVal: 96, val: "Mode 7"},
-        {chVal: 112, val: "Mode 8"},
-        {chVal: 128, val: "Mode 9"},
-        {chVal: 144, val: "Mode 10"},
-        {chVal: 160, val: "Mode 11"},
-        {chVal: 176, val: "Mode 12"},
-        {chVal: 192, val: "Mode 13"},
-        {chVal: 208, val: "Mode 14"},
-        {chVal: 224, val: "Mode 15"},
-        {chVal: 240, val: "Mode 16"},
-    ],
-};
-
-let soundActiveMode = {
-    channel: 10,
-    label: "Sound Active Mode",
-    condition: device => between(device.programMode.dmx, [[205, 255]]),
-    modes: [
-        {chVal: 0, val: "Mode 1"},
-        {chVal: 16, val: "Mode 2"},
-        {chVal: 32, val: "Mode 3"},
-        {chVal: 48, val: "Mode 4"},
-        {chVal: 64, val: "Mode 5"},
-        {chVal: 80, val: "Mode 6"},
-        {chVal: 96, val: "Mode 7"},
-        {chVal: 112, val: "Mode 8"},
-        {chVal: 128, val: "Mode 9"},
-        {chVal: 144, val: "Mode 10"},
-        {chVal: 160, val: "Mode 11"},
-        {chVal: 176, val: "Mode 12"},
-        {chVal: 192, val: "Mode 13"},
-        {chVal: 208, val: "Mode 14"},
-        {chVal: 224, val: "Mode 15"},
-        {chVal: 240, val: "Mode 16"},
-    ],
-};
-
-let speed = rangeProp({
-    channel: 11,
-    label: "Program speed",
-});
-let dimmerCurves = {
-    channel: 12,
-    label: "Dimmer Curves",
-    condition: device => device.programMode.dmx < 248,
-    modes: [
-        {chVal: 0, val: "Standard"},
-        {chVal: 21, val: "Stage"},
-        {chVal: 41, val: "TV"},
-        {chVal: 61, val: "Architectural"},
-        {chVal: 81, val: "Theatre"},
-        {chVal: 101, val: "Default"},
+        {val: 0, chVal: 63},
+        {val: 0.1, chVal: 64},
+        {val: 1, chVal: 95},
     ],
 };
 
@@ -199,26 +22,6 @@ let colorControl = {
     },
 };
 
-class Strobe extends Control {
-    get() {
-        // strobe universally should be: 0=off, 0.01..1 = flickering faster and faster
-        let chVal = this.strobe.chVal;
-        if (chVal < 64) {
-            return 0;
-        } else if (chVal <= 95) {
-            return (chVal - 63) / 32;
-        }
-    }
-
-    set(value) {
-        if (value == 0) {
-            this.strobe.dmx = 32;
-        } else {
-            this.strobe.dmx = 64 + Math.floor(value * 31);
-        }
-    }
-}
-
 function getPixels(controls) {
     return [{id: "light", label: "Light", controls}];
 }
@@ -231,74 +34,30 @@ export default ModelFactory({
     config: [
         {
             name: "6ch",
-            props: {red, green, blue, white, amber, uv},
-            pixels: getPixels({color: colorControl, amber: "amber", uv: "uv"}),
-        },
-        {
-            name: "7ch",
-            props: {red, green, blue, white, amber, uv, dimmer},
-            pixels: getPixels({color: colorControl, amber: "amber", uv: "uv", dimmer: "dimmer"}),
+            props: {
+                red: rangeProp({}),
+                green: rangeProp({}),
+                blue: rangeProp({}),
+                white: rangeProp({}),
+                amber: rangeProp({}),
+                uv: rangeProp({}),
+            },
+            pixels: getPixels({color: colorControl}),
         },
         {
             name: "8ch",
-            props: {red, green, blue, white, amber, uv, dimmer, strobe, strobeSpeed},
-            pixels: getPixels({
-                color: colorControl,
-                amber: "amber",
-                uv: "uv",
-                dimmer: "dimmer",
-                strobe: {type: Strobe, props: {strobe: "strobe"}},
-            }),
-        },
-        {
-            name: "11ch",
             props: {
-                red,
-                green,
-                blue,
-                white,
-                amber,
-                uv,
-                dimmer,
+                red: rangeProp({}),
+                green: rangeProp({}),
+                blue: rangeProp({}),
+                white: rangeProp({}),
+                amber: rangeProp({}),
+                uv: rangeProp({}),
+                brightness: rangeProp({defaultDMXVal: 255}),
                 strobe,
-                strobeSpeed,
-                programMode,
-                colorMacro,
-                colorChange,
-                colorFade,
-                soundActiveMode,
-                speed,
             },
             pixels: getPixels({
                 color: colorControl,
-                dimmer: "dimmer",
-                strobe: {type: Strobe, props: {strobe: "strobe"}},
-            }),
-        },
-        {
-            name: "12ch",
-            props: {
-                red,
-                green,
-                blue,
-                white,
-                amber,
-                uv,
-                dimmer,
-                strobe,
-                strobeSpeed,
-                programMode,
-                colorMacro,
-                colorChange,
-                colorFade,
-                soundActiveMode,
-                speed,
-                dimmerCurves,
-            },
-            pixels: getPixels({
-                color: colorControl,
-                dimmer: "dimmer",
-                strobe: {type: Strobe, props: {strobe: "strobe"}},
             }),
         },
     ],
--- src/pars/adj-mega-tri-par.js
+++ src/pars/adj-mega-tri-par.js
@@ -1,62 +1,16 @@
 import {ModelFactory} from "../device.js";
 import {rangeProp} from "../utils.js";
 
-function between(val, bounds) {
-    return bounds.some(([lower, upper]) => val >= lower && val <= upper);
-}
-
-let red = rangeProp({channel: 1, label: "Red"});
-let green = rangeProp({channel: 2, label: "Green"});
-let blue = rangeProp({channel: 3, label: "Blue"});
-let uv = rangeProp({channel: 4, label: "Ultraviolet"});
 let strobe = {
-    channel: 5,
+    // even though the tripar is capable of several strobe modes, we default to the standard
+    // as anything more sophisticated can be implemented on software side
     label: "Strobe",
-    modes: [
-        {chVal: 0, val: "off", label: "Off"},
-        {chVal: 32, val: "on", label: "On"},
-        {chVal: 64, val: "pulse", label: "Strobe"},
-        {chVal: 128, val: "pulse", label: "Pulse"},
-        {chVal: 192, val: "random", label: "Random"},
-    ],
-    defaultDMXVal: 32,
-};
-
-let strobeSpeed = {
-    channel: 5,
-    label: "Strobe Speed",
-    modifies: "strobe",
-    condition: device =>
-        between(device.strobe.dmx, [
-            [64, 95],
-            [128, 159],
-            [192, 223],
-        ]),
+    defaultDMXVal: 63,
     stops: [
-        {chVal: 0, val: 0, label: "Slow"},
-        {chVal: 31, val: 1, label: "Fast"},
+        {val: 0, chVal: 63},
+        {val: 0.1, chVal: 64},
+        {val: 1, chVal: 95},
     ],
-};
-
-let Strobe = {
-    props: {strobe: "strobe"},
-    get(props) {
-        // strobe universally should be: 0=off, 0.01..1 = flickering faster and faster
-        let chVal = props.strobe.chVal;
-        if (chVal < 64) {
-            return 0;
-        } else if (chVal <= 95) {
-            return (chVal - 63) / 32;
-        }
-    },
-
-    set(props, value) {
-        if (value == 0) {
-            props.strobe.dmx = 32;
-        } else {
-            props.strobe.dmx = 64 + Math.floor(value * 31);
-        }
-    },
 };
 
 let colorControl = {
@@ -70,10 +24,6 @@ let colorControl = {
     },
 };
 
-function getPixels(controls) {
-    return [{id: "light", label: "Light", controls}];
-}
-
 export default ModelFactory({
     model: "ADJ Mega TRI Par",
     widthCm: 20,
@@ -82,26 +32,42 @@ export default ModelFactory({
     config: [
         {
             name: "4ch",
-            props: {red, green, blue, uv},
-            pixels: getPixels({color: colorControl, uv: "uv"}),
-        },
-        {
-            name: "5ch",
-            props: {red, green, blue, uv, dimmer: rangeProp({channel: 5, label: "Dimmer", defaultDMXVal: 255})},
-            pixels: getPixels({color: colorControl, dimmer: "dimmer", uv: "uv"}),
+            props: {
+                red: rangeProp({}),
+                green: rangeProp({}),
+                blue: rangeProp({}),
+                uv: rangeProp({}),
+            },
+            pixels: [
+                {
+                    id: "light",
+                    label: "Light",
+                    controls: {
+                        color: colorControl,
+                        uv: "uv",
+                    },
+                },
+            ],
         },
         {
             name: "6ch",
             props: {
-                red,
-                green,
-                blue,
-                uv,
-                strobe,
-                strobeSpeed,
-                dimmer: rangeProp({channel: 6, label: "Dimmer", defaultDMXVal: 255}),
+                red: rangeProp({}),
+                green: rangeProp({}),
+                blue: rangeProp({}),
+                uv: rangeProp({}),
+                strobe: strobe,
+                brightness: rangeProp({defaultDMXVal: 255}),
             },
-            pixels: getPixels({color: colorControl, dimmer: "dimmer", uv: "uv", strobe: Strobe}),
+            pixels: [
+                {
+                    id: "light",
+                    label: "Light",
+                    controls: {
+                        color: colorControl,
+                    },
+                },
+            ],
         },
     ],
 });

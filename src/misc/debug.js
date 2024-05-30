b7d6680ef12b82a52658e32bb70349f847cbd3fd
Tom Striker <toms.baugis@gmail.com>
2024-05-30 13:51:04 +0100

    fix debug fixture - wasn't doing anything as it needed a pixel

--- src/misc/debug.js
+++ src/misc/debug.js
@@ -4,9 +4,14 @@ import {rangeProp} from "../utils.js";
 function channels(i) {
     let channelProps = {};
     for (let channel = 1; channel <= i; channel++) {
-        channelProps[`ch${channel}`] = rangeProp({label: `Ch ${channel}`});
+        channelProps[`ch${channel < 10 ? "0" + channel : channel}`] = rangeProp({
+            label: channel < 10 ? `0${channel}` : channel,
+        });
     }
-    return channelProps;
+    return {
+        props: channelProps,
+        pixels: [{id: "dummy"}], // needs a dummy pixel to attach to, or otherwise the code doesn't recognise it
+    };
 }
 
 export default ModelFactory({
@@ -14,37 +19,13 @@ export default ModelFactory({
     model: "Debug Device",
     type: "debug",
     config: [
-        {
-            name: "1ch",
-            props: channels(1),
-        },
-        {
-            name: "2ch",
-            props: channels(2),
-        },
-        {
-            name: "4ch",
-            props: channels(4),
-        },
-        {
-            name: "8ch",
-            props: channels(8),
-        },
-        {
-            name: "16ch",
-            props: channels(16),
-        },
-        {
-            name: "32ch",
-            props: channels(32),
-        },
-        {
-            name: "64ch",
-            props: channels(64),
-        },
-        {
-            name: "256ch",
-            props: channels(256),
-        },
+        {name: "1ch", ...channels(1)},
+        {name: "2ch", ...channels(2)},
+        {name: "4ch", ...channels(4)},
+        {name: "8ch", ...channels(8)},
+        {name: "16ch", ...channels(16)},
+        {name: "32ch", ...channels(32)},
+        {name: "64ch", ...channels(64)},
+        {name: "256ch", ...channels(256)},
     ],
 });

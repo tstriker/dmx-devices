d74423ef5cd2e9ba51cb755b7c916cc7d39d31e8
Tom Striker <toms.baugis@gmail.com>
2025-08-13 11:01:29 +0100

    move to config

--- src/par-bars/cosmos-108.js
+++ src/par-bars/cosmos-108.js
@@ -1,59 +1,20 @@
-import {ModelFactory} from "../device.js";
-import {rangeProp} from "../utils.js";
+import {parseFixtureConfig} from "../parser.js";
 
-function rgb(...addresses) {
-    // generates the rgb props triplet and colors pixel
-    let config = {props: {}, pixels: []};
-
-    addresses.forEach((address, idx) => {
-        // we want to start with 1, not 0
-        idx += 1;
-
-        config.props = {
-            ...config.props,
-            [`red${idx}`]: rangeProp({channel: address + 0, label: `Red ${idx}`}),
-            [`green${idx}`]: rangeProp({channel: address + 1, label: `Green ${idx}`}),
-            [`blue${idx}`]: rangeProp({channel: address + 2, label: `Blue ${idx}`}),
-        };
-
-        config.pixels.push({
-            id: `light${idx}`,
-            label: `Light ${idx}`,
-            controls: {
-                color: {
-                    type: "rgb-light",
-                    props: {
-                        red: `red${idx}`,
-                        green: `green${idx}`,
-                        blue: `blue${idx}`,
-                    },
-                },
-            },
-        });
-    });
-
-    return config;
-}
-
-function strobe(channel, label) {
-    return rangeProp({label: "Strobe"});
-}
-
-export default ModelFactory({
+export default parseFixtureConfig({
     model: `Cosmos 108`,
     widthCm: 60,
     type: "par-bar",
-    config: [
+    modes: [
         {
             name: "15ch",
-            lights: 4,
-            props: {
-                mode: rangeProp({channel: 1, label: "Mode", ui: false}),
-                dimmer: rangeProp({channel: 2, label: "Dimmer", activeDefault: 255}),
-                strobe: rangeProp({channel: 3, label: "Strobe"}),
-                ...rgb(4, 7, 10, 13).props,
-            },
-            pixels: [...rgb(4, 7, 10, 13).pixels],
+            props: [
+                {type: "custom", label: "mode", ui: false},
+                {type: "dimmer", activeDefault: 255},
+                "strobe",
+                {type: "red", repeats: 4, every: 3},
+                {type: "green", repeats: 4, every: 3},
+                {type: "bluu", repeats: 4, every: 3},
+            ],
         },
     ],
 });
--- src/pars/adj-mega-tri-par.js
+++ src/pars/adj-mega-tri-par.js
@@ -1,73 +1,35 @@
-import {ModelFactory} from "../device.js";
-import {rangeProp} from "../utils.js";
+import {parseFixtureConfig} from "../parser.js";
 
-let strobe = {
-    // even though the tripar is capable of several strobe modes, we default to the standard
-    // as anything more sophisticated can be implemented on software side
-    label: "Strobe",
-    modes: [
-        {chVal: 32, val: "Don't strobe"},
-        {chVal: 64, val: "Strobe slow-fast", range: 32},
-        {chVal: 128, val: "Pulse strobe", range: 32},
-        {chVal: 192, val: "Random strobe", range: 32},
-    ],
-    defaultVal: 32,
-};
-
-let colorControl = {
-    name: "light",
-    type: "rgb-light",
-    label: "Light",
-    props: {
-        red: "red",
-        green: "green",
-        blue: "blue",
-    },
-};
-
-export default ModelFactory({
+export default parseFixtureConfig({
     model: "ADJ Mega TRI Par",
     widthCm: 20,
     type: "rgb-light",
 
-    config: [
+    modes: [
         {
             name: "4ch",
-            props: {
-                red: rangeProp({}),
-                green: rangeProp({}),
-                blue: rangeProp({}),
-                uv: rangeProp({}),
-            },
-            pixels: [
-                {
-                    id: "light",
-                    label: "Light",
-                    controls: {
-                        color: colorControl,
-                        uv: "uv",
-                    },
-                },
-            ],
+            props: ["red", "green", "blue", "uv"],
         },
         {
             name: "6ch",
-            props: {
-                red: rangeProp({}),
-                green: rangeProp({}),
-                blue: rangeProp({}),
-                uv: rangeProp({}),
-                strobe: strobe,
-                brightness: rangeProp({label: "Dimmer (always on)", activeDefault: 255, ui: false}),
-            },
-            pixels: [
+            props: [
+                "red",
+                "green",
+                "blue",
+                "uv",
                 {
-                    id: "light",
-                    label: "Light",
-                    controls: {
-                        color: colorControl,
-                    },
+                    // even though the tripar is capable of several strobe modes, we default to the standard
+                    // as anything more sophisticated can be implemented on software side
+                    type: "strobe",
+                    modes: [
+                        {chVal: 32, val: "Don't strobe"},
+                        {chVal: 64, val: "Strobe slow-fast", range: 32},
+                        {chVal: 128, val: "Pulse strobe", range: 32},
+                        {chVal: 192, val: "Random strobe", range: 32},
+                    ],
+                    defaultVal: 32,
                 },
+                {type: "dimmer", activeDefault: 255, ui: false},
             ],
         },
     ],
